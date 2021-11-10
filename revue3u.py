c89e7bbb7d85f77c2fa377fc3ae0782ef46dea8a
lito-leo <dufourlp@gmail.com>
2021-11-10 13:31:00 -0500

    changes initial

---
+++ revue3u.py
@@ -0,0 +1,38 @@
+#revue 3u - python
+
+
+#constantes
+
+BEAUTIFUL = "~~~~~~~~~~~~~"
+TITRE = "Menu Alimentaire"
+
+choix = [
+	"pomme", 
+	"biscuit", 
+	"chocolat", 
+    "pizza"
+	]
+
+#fonctions sur mesure
+#logique de l'application
+#afficher le menu
+
+print(BEAUTIFUL + TITRE + BEAUTIFUL)  
+print()
+
+for i in range(0, len(choix)) :
+	print(f"{i} - {choix[i]}")
+
+#prendre le choix de l'utilisateur
+
+
+
+#saisir le choix et voir si le choix est valide
+
+
+
+#enfin...?
+
+
+
+#encore...?
