97ef607e315ea3a2ac635326a09484f8fb5fd27a
Leo <dufourlp@gmail.com>
2021-11-17 11:53:15 -0500

    Forgetful machine (ex7)

---
+++ Forgetfulmachine.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Forgetfulmachine 
+{
+    public static void main( String[] args ) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println( "What city is the capital of France?" );
+        keyboard.next();
+
+        System.out.println( "What is 6 multiplied by 7?" );
+        keyboard.nextInt();
+
+        System.out.println( "Enter a number between 0.0 and 1.0." );
+        keyboard.nextDouble();
+
+        System.out.println( "Is there anything else you would like to say?" );
+        keyboard.next();
+
+        keyboard.close();
+    }
+}
+
+//Quand ont met un float dans la question 2 le prgramme faillit
+//Quand ont met un string dans la question 3 le programme faillit
