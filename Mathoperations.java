e65fcd7394363bf9fe288064dff11972915d8575
Leo <dufourlp@gmail.com>
2021-11-17 11:42:23 -0500

    Math operations (ex6)

---
+++ Mathoperations.java
@@ -0,0 +1,42 @@
+public class Mathoperations 
+{
+    public static void main ( String [] args)
+    {
+        int a, b, c, d, e, f, g, h;
+        double x, y, z, i;
+        String one, two, both;
+        
+        a = 10;
+        b = 27;
+        System.out.println( "a is " + a + ", b is " + b );
+    
+        c = a + b;
+        System.out.println( "a+b is " + c );
+        d = a - b;
+        System.out.println( "a-b is " + d );
+        e = a+b*3;
+        System.out.println( "a+b*3 is " + e );
+        f = b / 2;
+        System.out.println( "b/2 is " + f );
+        g = b % 10;
+        System.out.println( "b%10 is " + g );
+        
+        x = 1.1;
+        System.out.println( "\nx is " + x );
+        y = x*x;
+        System.out.println( "x*x is " + y );
+        z = b / 2;
+        System.out.println( "b/2 is " + z );
+        System.out.println();
+        
+        one = "dog";
+        two = "house";
+        both = one + two;
+        System.out.println( both );
+        i = (((b - a) / x ) % 4) * 2;
+        h = 5;
+        System.out.println(i * h);
+
+    }
+    
+}
