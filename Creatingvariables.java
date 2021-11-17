e9bdb85078ef4286a89de1410272f09297007d01
Leo <dufourlp@gmail.com>
2021-11-17 11:28:17 -0500

    Creating variables (ex5)

---
+++ Creatingvariables.java
@@ -0,0 +1,33 @@
+public class Creatingvariables 
+{
+    public static void main (String [] args)
+    {
+        int x, y, z;
+        double seconds, e, checking, pi;
+        String firstName, lastName, title, plus_one, plus_two;
+
+        x = 10;
+        y = 400;
+        z = 2000;
+
+        seconds = 4.71;
+        e = 2.71828182845904523536;
+        checking = 1.89;
+        pi = 3.14159;
+        firstName = "Leo";
+        lastName = "Dufour";
+        title = "";
+        plus_one = "Insert";
+        plus_two = "Word";
+
+        System.out.println("La variable x :" + x);
+        System.out.println("La valeur " + y + " est dans la variable y.");
+        System.out.println("L'expérience à pris " + seconds + "secondes.");
+        System.out.println("Un nombre irrationel populaire, celui de Euler: " + e);
+        System.out.println("Tu devrais avoir plus de $" + checking + "!");
+        System.out.println("Mon nom est:" + title + firstName + " " + lastName);
+        System.out.println("Ajouts:" + z + pi + plus_one + plus_two);
+
+    }
+    
+}
