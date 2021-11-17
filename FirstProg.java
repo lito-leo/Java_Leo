545b9cb30a36f82db2dff75288883ea70ca4af43
Leo <dufourlp@gmail.com>
2021-11-17 10:00:13 -0500

    FirstProg

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
     public static void main ( String[] args ) {
-        System.out.println( "Je vais apprendre a coder!" );
-        System.out.println( "Aujourd'hui c'est:" );
+        System.out.println( "Moi (Leo Dufour) vais apprendre a coder!" );
+        System.out.println( "Aujourd'hui c'est le 17 Novembre 2021" );
     }
 }
